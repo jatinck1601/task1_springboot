fa7f3c1394b23f97ae8430656c0c9f98028c312b
csecec.1803167@gmail.com <csecec.1803167@gmail.com>
2021-11-15 17:26:19 +0530

    Task1 SpringBoot Commit

---
+++ src/main/java/com/mkj/rapipay/web/RapipayClientWebLayer.java
@@ -0,0 +1,40 @@
+package com.mkj.rapipay.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mkj.rapipay.service.IRapipayClientService;
+
+@RestController
+@RequestMapping("/client")
+public class RapipayClientWebLayer {
+
+	@Autowired
+	private IRapipayClientService rapipayServiceImpl;
+	
+	public RapipayClientWebLayer() {
+		super();
+		// TODO Auto-generated constructor stub
+		System.out.println("constructor called //");
+	}
+
+	@RequestMapping("/hello")
+	public String helloEndPoint()
+	{
+		return "Rapipay client Hello";
+	}
+	@RequestMapping("/sum/{num1}/{num2}") 
+	public int getSum(@PathVariable(name = "num1") Integer a, @PathVariable(name = "num2") Integer b) {
+		return a + b;
+	}
+
+	@RequestMapping("/addition")
+	public int getaddition(@RequestParam int num1,@RequestParam int num2) {
+		return num1+num2;
+	}
+	
+	
+}
